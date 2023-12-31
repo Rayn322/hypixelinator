464f11fe890e3eace07d1ff5248714306a7c2079
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-12-31 15:47:49 -0800

    Init shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1 class="text-xl text-blue-800">Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<h1 class="text-xl">Welcome to SvelteKit</h1>
 <p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	Visit <a class="text-" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
+	the documentation
 </p>
+
+<Button>Click me</Button>
+<div class="w-96 p-4">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Card Title</Card.Title>
+			<Card.Description>Card Description</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<p>Card Content</p>
+		</Card.Content>
+		<Card.Footer>
+			<p>Card Footer</p>
+		</Card.Footer>
+	</Card.Root>
+</div>
